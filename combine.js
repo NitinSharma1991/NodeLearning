b787f5d2751ad5912425ab33f60fcd1a393cfe97
sharm <nitin.rebelee91@gmail.com>
2019-10-19 00:44:33 +0530

    "Git Code"

---
+++ combine.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+function callAPI(id) {
+    return new Promise((resolve, reject) => {
+        var options = {
+            host: 'example.com',
+            path: '/q/result/' + id
+        }
+
+        http.get(options, (res) => {
+            var body = [];
+            res.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                resolve(body);
+            }).on('error', reject);
+        });
+    });
+}
+
+function combine(inputs) {
+    return Promise.all(inputs.map(input => callAPI(input.id)))
+        // completed array of bodies
+        // console.log(combined);
+        // foo(combined.length, [...args]);
+        ;
+}
+
+combine([1, 2]).then((combine) => {
+    console.log(combine)
+}).catch((error) => {
+        console.log(error)
+    }
+);
+
